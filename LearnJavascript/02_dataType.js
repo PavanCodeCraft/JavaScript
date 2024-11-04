59297f4bbcedea97f832dc2a432ba378f37ec636
Pavan Singh <152471729+PavanCodeCraft@users.noreply.github.com>
2024-11-04 16:23:58 +0000

    datatypes

---
+++ LearnJavascript/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict" // treat all code a newer version
+
+// alert (3+4) if we run this we will get error because this is nodejs not browser
+
+// note: code readablity should be high 
+
+// DataType
+
+// Number
+// BigInt
+// String
+// boolean => ture/false
+// null => standalon value
+// undefined
+//symbol => umique
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
