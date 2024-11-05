73c0a5d3e58b7d448e686e692743c67b06a5dea4
Pavan Singh <152471729+PavanCodeCraft@users.noreply.github.com>
2024-11-05 04:04:25 +0000

    discuss comparison operators

---
+++ LearnJavascript/comparison.js
@@ -0,0 +1,42 @@
+// Operators
+
+// comparison operators
+// equal (==)
+// not equal (!=)
+// strict equal (===)
+// greater than (>)
+// less than(<)
+// greater than equal (>=)
+// less than equal (<=)
+
+// console.log(2 > 3);
+// console.log(2 < 3);
+// console.log(2 == 3);
+// console.log(2 != 3);
+// console.log(2 === 2);
+
+//similarly we can compare and do more experiments and above all the of the same datatye so it is easy no need to worry
+
+// Comparison between different datatype 
+
+// console.log("2" == 2);  //return true  (js convert "2" into number and then compare but comparing two different datatype are not recommandad it may return random output)
+
+// console.log("2" === 2); // return false because it is strict  check
+// console.log("2" != 3); // return true
+
+// let x = null
+// const y = Number(x)
+// console.log(typeof(x));
+// console.log (typeof(y));
+
+// console.log(null > 2); //return false
+// console.log(null > 0);  //return false
+// console.log(null > -1);  //return true
+// console.log(null >=0 );  //return ture
+// console.log(null == 0);  //return false
+// console.log(null === 0);  // return false 
+ 
+// Above are not good practice and not recommanded
+
+
+
